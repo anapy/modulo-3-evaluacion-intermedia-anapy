94f02db0d21424cf14b2f155722273c145857d66
anapy <anabelenfunesc@gmail.com>
2020-07-28 11:33:17 +0200

    adding styles to cards

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -4,7 +4,7 @@ import Pokemon from './Pokemon';
 const PokeList = props => {
   const pokemons = props.pokemons.map((pokemon, index) => {
     return (
-      <li key={index}>
+      <li className="pokemon_container" key={index}>
         <Pokemon name={pokemon.name} types={pokemon.types} img={pokemon.url}/>
       </li>
     )
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Pokemon = props => {
   const types = props.types.map((type, index) => {
-    return <li key={index}>{type}</li>
+    return <li className="type" key={index}>{type}</li>
   }
   );
     return (
-    <article>
-    <h2>{props.name}</h2>
-    <div><img src={props.img} alt={props.name}/></div>
-    <ul className="types">{types}</ul>
+    <article className="pokemon">
+      <div className="photo_container"><img className="img" src={props.img} alt={props.name}/></div>
+      <h2 className="name">{props.name}</h2>
+      <ul className="types">{types}</ul>
     </article>
     );
 }
