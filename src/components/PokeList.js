6405f41f117ab5c73b304214480e38671e961495
anapy <anabelenfunesc@gmail.com>
2020-07-28 19:06:35 +0200

    adding more styles

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -4,7 +4,7 @@ import Pokemon from './Pokemon';
 const PokeList = props => {
   const pokemons = props.pokemons.map((pokemon) => {
     return (
-      <li className="pokemon_container" key={pokemon.id}>
+      <li className="pokemon_container animate__animated animate__bounce" key={pokemon.id}>
         <Pokemon name={pokemon.name} types={pokemon.types} img={pokemon.url}/>
       </li>
     )
